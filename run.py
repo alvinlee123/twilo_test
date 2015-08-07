906610585e65e8b723ffa25723b283328afc6cdc
Alvin <alvinlee123@hotmail.com>
2015-08-07 02:11:15 -0700

    modifed run

--- run.py
+++ run.py
@@ -1,15 +1,37 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import twilio.twiml
 
 app = Flask(__name__)
 
-@app.route("/",methods=['GET','POST'])
-def hello_monkey():
-	"""Responds to incoming requests."""
-	resp = twilio.twiml.Response()
-	resp.say("Hello Monkey")
+callers = {
+    "+15107120735": "Alvin Lee",
+    }
 
-	return str(resp)
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming requests."""
+    from_number = request.values.get('From',None)
+    resp = twilio.twiml.Response()
+
+    if from_number in callers:
+        #Greet caller by name
+        resp.say("Hello " + callers[from_number])
+    else:
+        resp.say("FUCK YOU")
+
+    return str(resp)
+
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
 
